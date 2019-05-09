92d99ffae5bbf77b053501b09cd5805f1970196a
professor <professor@L2MP.IPIRANGA.FATEC>
2019-05-09 15:55:45 -0300

    Inclusão dos testes JPA

---
+++ scel/src/main/java/com/fatec/scel/model/Usuario.java
@@ -0,0 +1,58 @@
+package com.fatec.scel.model;
+
+import java.util.Set;
+import java.util.UUID;
+import javax.persistence.*;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+	@Id
+	private UUID id;
+	@NotEmpty(message = "O email deve ser preenchido")
+	private String email;
+	@Size(min = 1, max = 4, message = "Senha deve ter no maximo 4 caracteres")
+	private String password;
+	@ElementCollection(fetch = FetchType.EAGER)
+	@Enumerated(EnumType.STRING)
+	@NotNull
+	private Set<PapelDoUsuario> papel;
+
+	protected Usuario() {
+	}
+
+	public Usuario(UUID id, String email, String password, Set<PapelDoUsuario> papel) {
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.papel = papel;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public Set<PapelDoUsuario> getPapel() {
+		return papel;
+	}
+
+	public String toString() {
+		return getEmail();
+	}
+}
---
+++ scel/src/test/java/com/fatec/scel/UC02CadastrarUsuario.java
@@ -0,0 +1,48 @@
+package com.fatec.scel;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import com.fatec.scel.model.PapelDoUsuario;
+import com.fatec.scel.model.Usuario;
+import com.fatec.scel.model.UsuarioRepository;
+import java.util.HashSet;
+import java.util.UUID;
+
+
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UC02CadastrarUsuario {
+	@Autowired
+	private UsuarioRepository repository;
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	private Usuario usuario;
+
+	@Before
+	public void start() {
+		HashSet<PapelDoUsuario> papel = new HashSet<>();
+		papel.add(PapelDoUsuario.PROFESSOR);
+		usuario = new Usuario(UUID.randomUUID(), "prof.edsonalmeida@fatec.sp.br", "pwd", papel);
+	}
+	@Test
+	public void ct01_testStoreUser() {
+		HashSet<PapelDoUsuario> papel = new HashSet<>();
+		papel.add(PapelDoUsuario.PROFESSOR);
+		repository.save(new Usuario(UUID.randomUUID(), "prof.edsonalmeida@fatec.sp.br", "pwd", papel));
+		assertThat(repository.count()).isEqualTo(1L);
+	}
+	
+}
